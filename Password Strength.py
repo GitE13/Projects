c4b8c9a15f39e65f877644a6271d0fc0ecfdb038
GitE13 <elisandersfields@gmail.com>
2026-04-14 09:55:07 -0400

    Made it better

--- Password Strength.py
+++ Password Strength.py
@@ -1,31 +1,43 @@
 import math
 from wordfreq import top_n_list
-textwidth = 0
-pressedkey = [None, 0]
-        
+from passwordlib.commonly_used import is_commonly_used
+
+
 def drawpasswordguess(password):
     modifiers = ['Nanoseconds', 'Microseconds', 'Milliseconds', 'Seconds', 'Minutes', 'Hours', 'Days', 'Weeks', 'Months', 'Years', 'Decades', 'Centuries', 'Millennia']
     times = [1/1e9, 1/1e6, 1/1e3, 1, 60, 3600, 86400, 604800, 2629800, 31557600, 315576000, 3155760000, 31557600000]
     strength = getstrength(password,times)
-    return strength[0], modifiers[strength[1]], strength[2]
+    return strength[0], modifiers[strength[1]], strength[2], strength[3]
     
 def getstrength(password:str,times):
-    if password in top_n_list('en', 100000):
-        return (1, 0, 1/1e9)
-    if password[0].lower()+password[1:] in top_n_list('en', 100000):
-        return (1, 0, 1/1e9)
+    issues = []
     chars = 26
     if any(i.isupper() for i in password):
         chars += 26
+    else:
+        issues.append('No uppercase letters')
     if any(i.isdigit() for i in password):
         chars += 10
-    if any(i in '!@#$%^&*()-+' for i in password):
-        chars += 12
+    else:
+        issues.append('No numbers')
+    if any(i in '!@#$%^&*()-+<>?,.[]{}=_|/~`' for i in password):
+        chars += len('!@#$%^&*()-+<>?,.[]{}=_|/~`')
+        print(len('!@#$%^&*()-+<>?,.[]{}=_|/~`'))
+    else:
+        issues.append('No special characters')
+    if len(password) < 8:
+        issues.append('Password is less than 8 characters long')
+    if password in top_n_list('en', 100000):
+        return (1, 1, 1/1e9, issues+['Is a common word (change immediately)'])
+    if password[0].lower()+password[1:] in top_n_list('en', 100000):
+        return (1, 1, 1/1e9, issues+['Is a common word (change immediately)'])
+    if is_commonly_used(password):
+        return (1, 1, 1/1e9, issues+['Is a common password (change immediately)'])
     strength = (chars ** len(password)) / 1e9
     index = 0
     while index < len(times) and strength >= times[index]:
         index += 1
-    return (strength/times[index-1], index-1,strength)
+    return (strength/times[index-1], index-1,strength, issues)
 
 def formattext(text):
     if text < 1e10:
@@ -43,7 +55,11 @@ if __name__ == "__main__":
         if inp == 'exit':
             running = False
         else:
-            strength, modifier, rawstrength = drawpasswordguess(inp)
+            strength, modifier, rawstrength, issues = drawpasswordguess(inp)
             print(f"Time to break password: {formattext(strength)} {modifier}")
             score = round(math.log(rawstrength,1000)+3,2)
             print(f'Your password score is {score}'+(' (very weak)' if score < 3 else ' (weak)' if score < 5 else ' (okay)' if score < 6 else ' (strong)' if score < 6.5 else ' (very strong)'))
+            if issues:
+                print('Issues found with your password:')
+                for issue in issues:
+                    print(f' - {issue}')
